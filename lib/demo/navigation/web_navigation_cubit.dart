31ea88e7540e44493dce6c2a6de1286050bbc105
Craig Merry <craigm26@gmail.com>
2025-06-28 22:28:23 -0700

    Fix compile errors and add missing cubit

--- lib/demo/navigation/web_navigation_cubit.dart
+++ lib/demo/navigation/web_navigation_cubit.dart
@@ -1,1 +1,43 @@
-// TODO Implement this library.
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+/// Sections available in the web demo page.
+enum WebSection { home, sound, localization, visual, settings, technology, about }
+
+/// State for [WebNavigationCubit].
+class WebNavigationState {
+  final bool isDemoActive;
+  final WebSection currentSection;
+
+  const WebNavigationState({
+    this.isDemoActive = false,
+    this.currentSection = WebSection.home,
+  });
+
+  WebNavigationState copyWith({bool? isDemoActive, WebSection? currentSection}) {
+    return WebNavigationState(
+      isDemoActive: isDemoActive ?? this.isDemoActive,
+      currentSection: currentSection ?? this.currentSection,
+    );
+  }
+}
+
+/// Simple cubit used to navigate between sections of the web demo and to
+/// toggle demo mode.
+class WebNavigationCubit extends Cubit<WebNavigationState> {
+  WebNavigationCubit() : super(const WebNavigationState());
+
+  /// Navigate to a specific section.
+  void navigateToSection(WebSection section) {
+    emit(state.copyWith(currentSection: section));
+  }
+
+  /// Mark the demo as started.
+  void startDemo() {
+    emit(state.copyWith(isDemoActive: true));
+  }
+
+  /// Stop the demo.
+  void stopDemo() {
+    emit(state.copyWith(isDemoActive: false));
+  }
+}
--- lib/demo/widgets/hero_section.dart
+++ lib/demo/widgets/hero_section.dart
@@ -1,4 +1,3 @@
-import '../../../demo/web_navigation_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../navigation/web_navigation_cubit.dart';
