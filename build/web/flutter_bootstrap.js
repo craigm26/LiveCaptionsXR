f434cca17cafd6d08a723d0362c2c8c29c1312ef
Craig Merry <craigm26@gmai.com>
2025-07-18 15:56:42 -0700

    Implement code changes to enhance functionality and improve performance

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2070370750"
+    serviceWorkerVersion: "1641494560"
   }
 });
