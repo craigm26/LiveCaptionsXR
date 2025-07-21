5aa923893f7689cf686c619546e1b898e4ef5225
Craig Merry <craigm26@gmai.com>
2025-07-21 15:06:39 -0700

    fix: Update service worker version and resource hashes in web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "34524f809cc996916b66cbc75614cc47",
+const RESOURCES = {"flutter_bootstrap.js": "effbe759620fbc7c070954ef81875008",
 "version.json": "a4d6fdab345c0aa1210f95627e23870d",
 "index.html": "ae5e58be2dbfccd62fca4497de4baba6",
 "/": "ae5e58be2dbfccd62fca4497de4baba6",
